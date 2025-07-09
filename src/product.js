e6be7d3b0f7d91b08520a270d2c4dc8ef5d4c992
aya-othmen7 <ayaothmen7@gmail.com>
2025-07-09 15:21:49 +0100

    create product.js

---
+++ src/product.js
@@ -0,0 +1,9 @@
+// src/product.js
+const product = {
+  name: "Smartphone",
+  price: "$799",
+  description: "A powerful smartphone with excellent performance and battery life.",
+  image: "https://m.media-amazon.com/images/I/61aiFCe6PpL._AC_SL1500_.jpg"
+};
+
+export default product;
